ef4a78ca3535aac4ab877b000ea5a1380885b45c
Aabhas Jain <aabhasmnit@gmail.com>
2022-01-16 01:19:12 +0530

    Rate Limiter Implementation

---
+++ RateLimiter/src/main/java/RateLimitListener.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author Aabhas
+ * Interface to be implemented by the application to receive
+ * notifications whenever the rate limit is breached.
+ * Future behavior can be added to this interface.
+ *
+ */
+public interface RateLimitListener
+{
+    public void rateLimitThresholdBreached();
+}
